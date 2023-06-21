ef274b2aea471e9a6482a715df7fea93212ac5b0
giahuyday <huyhoangnguyen301202@gmail.com>
2023-06-21 16:46:43 +0700

    Login and SignUp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // create App.js file
 import React from 'react'
-
+import Login from './pages/Login/Login'
+import SignUp from "./pages/Sign Up/SignUp"
 function App() {
     return (
         <div className="flex h-screen justify-center items-center">
             <h1 className="text-red-500 text-3xl">Hello Team dd</h1>
         </div>
+        // <Login></Login>
+        // <SignUp/>
     )
 }
 
