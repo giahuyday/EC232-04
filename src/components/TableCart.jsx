dd782351d9306bd58829e879aab917f5924ce760
your_username <83014500+dangminh71220002@users.noreply.github.com>
2023-06-25 15:35:41 +0700

    Cart UI

---
+++ src/components/TableCart.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+const TableCart = () => {
+    const [amount, setAmount] = useState(1);
+    const handleMinus = () => {
+        if (amount == 1) return 
+        setAmount(amount-1);
+    }
+    const handleAdd = () => {
+        setAmount(amount+1);
+    }
+    return (
+        <div className="flex flex-wrap items-center mb-6 -mx-4 md:mb-8 border-[2px] rounded-[7px]">
+        <div className="w-full px-4 my-3 md:w-4/6 lg:w-6/12 md:mb-0">
+            <div className="flex flex-wrap items-center -mx-4">
+                <div className="w-full px-4 mb-3 md:w-1/3">
+                    <div className="w-full h-96 md:h-24 md:w-24">
+                        <img src="https://i.postimg.cc/kGjz3dpD/pexels-cottonbro-3296434.jpg" alt="" className="object-cover w-full h-full rounded-[5px]" />
+                    </div>
+                </div>
+                <div className="w-2/3 px-4">
+                    <h2 className="mb-2 text-xl font-bold ">DSL Camera</h2>
+                    <p className="text-gray-500  ">Picture frame</p>
+                </div>
+            </div>
+        </div>
+        <div className="hidden px-4 lg:block lg:w-2/12">
+            <p className="text-lg font-bold text-blue-500 ">$99.00</p>
+            <span className="text-xs text-gray-500 line-through ">$1500</span>
+        </div>
+        <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
+            <div className="inline-flex items-center px-4 font-semibold text-gray-500   border-[2px] rounded-md  ">
+                <button className="py-2 hover:text-gray-700 flex justify-center" onClick={handleMinus}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-dash" viewBox="0 0 25 25">
+                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"></path>
+                    </svg>
+                </button>
+                <input type="number" className="w-12 px-2 py-4 text-center border-0 rounded-md bg-gray-100  md:text-right" placeholder="1" value={amount} />
+                <button className="py-2 hover:text-gray-700" onClick={handleAdd} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus" >
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
+            <p className="text-lg font-bold text-blue-500 ">$99.00</p>
+        </div>
+    </div>
+    );
+}
+export default TableCart;
--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -17,7 +17,7 @@ const Detail = () => {
     }
     return (
         <>
-            <section className="overflow-hidden bg-white py-11 font-poppins dark:bg-gray-800 my-[40px]">
+            <section className="overflow-hidden bg-white py-11 font-poppins  my-[40px]">
                 <div className="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6 rounded-[20px]">
                     <div className="flex flex-wrap -mx-4">
                         <div className="w-full mb-8 md:w-1/2 md:mb-0">
@@ -55,16 +55,16 @@ const Detail = () => {
                                 <div className="flex flex-wrap items-center mt-6">
                                     <span className="mr-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                            className="w-4 h-4 text-gray-700 dark:text-gray-400 bi bi-truck"
+                                            className="w-4 h-4 text-gray-700   bi bi-truck"
                                             viewBox="0 0 16 16">
                                             <path
                                                 d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z">
                                             </path>
                                         </svg>
                                     </span>
-                                    <h2 className="text-lg font-bold text-gray-700 dark:text-gray-400">Free Shipping</h2>
+                                    <h2 className="text-lg font-bold text-gray-700  ">Free Shipping</h2>
                                 </div>
-                                <div className="px-6 pb-6 mt-6 border-t border-gray-300 dark:border-gray-400 ">
+                                <div className="px-6 pb-6 mt-6  border-gray-300  border-t-[2px] ">
 
                                     <div className="mt-2 px-7">
                                         <h1 className="text-[16px] font-[600]">Parameter</h1>
@@ -95,7 +95,7 @@ const Detail = () => {
                                         </button>
                                     </div>
                                 </div>
-                                <div className="px-6 pb-6 mt-6 border-t border-gray-300 dark:border-gray-400 ">
+                                <div className="px-6 pb-6 mt-6 border-t border-gray-300   ">
 
                                     <div className="mt-2 px-7">
                                         <h1 className="text-[16px] font-[600]">About this item</h1>
@@ -132,32 +132,32 @@ const Detail = () => {
                             <div className="lg:pl-20">
                                 <div className="mb-8 ">
                                     <div className="border-b border-[black] bio">
-                                        <h2 className="max-w-xl  mb-[10px] text-[24px] font-bold dark:text-gray-400 md:text-4xl">
+                                        <h2 className="max-w-xl  mb-[10px] text-[24px] font-bold   md:text-4xl">
                                             Macbook Pro M130c90
                                         </h2>
 
                                         <div class="flex items-center">
                                             <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                                            <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
-                                            <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                                            <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">73 reviews</a>
+                                            <p class="ml-2 text-sm font-bold text-gray-900 ">4.95</p>
+                                            <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full "></span>
+                                            <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline ">73 reviews</a>
                                         </div>
 
                                     </div>
 
-                                    <p className="pt-[30px] inline-block mb-6 text-4xl font-bold text-gray-700 dark:text-gray-400 ">
+                                    <p className="pt-[30px] inline-block mb-6 text-4xl font-bold text-gray-700   ">
                                         <span className="text-[black]">$1500.99</span>
                                         <span
-                                            className="text-base font-normal text-gray-500 line-through dark:text-gray-400">$1800.99</span>
+                                            className="text-base font-normal text-gray-500 line-through  ">$1800.99</span>
                                     </p>
-                                    <p className="max-w-md text-gray-700 dark:text-gray-400">
+                                    <p className="max-w-md text-gray-700  ">
                                         Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
                                         Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
                                     </p>
                                 </div>
                                 <div className="mb-8">
                                     <h2
-                                        className="w-16 pb-1 mb-4 text-2xl font-bold border-b border-blue-300 dark:text-gray-400 dark:border-gray-600">
+                                        className="w-16 pb-1 mb-4 text-2xl font-bold border-b border-blue-300    ">
                                         Colors</h2>
                                     <div className="flex flex-wrap -mx-2 -mb-2">
                                         <button className="p-1 mb-2 mr-3 ">
@@ -173,20 +173,20 @@ const Detail = () => {
                                 </div>
                                 <div className="mb-8 ">
                                     <h2
-                                        className="w-16 pb-1 mb-4 text-xl font-semibold border-b border-blue-300 dark:border-gray-600 dark:text-gray-400">
+                                        className="w-16 pb-1 mb-4 text-xl font-semibold border-b border-blue-300    ">
                                         RAM</h2>
                                     <div>
                                         <div className="flex flex-wrap -mb-2">
                                             <button
-                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400   hover:text-blue-600    ">
                                                 8 GB
                                             </button>
                                             <button
-                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600      ">
                                                 16 GB
                                             </button>
                                             <button
-                                                className="px-4 py-2 mb-2 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600      ">
                                                 1 TB
                                             </button>
                                         </div>
@@ -194,20 +194,20 @@ const Detail = () => {
                                 </div>
                                 <div className="mb-8">
                                     <h2
-                                        className="w-16 pb-1 mb-6 text-xl font-semibold border-b border-blue-300 dark:border-gray-600 dark:text-gray-400">
+                                        className="w-16 pb-1 mb-6 text-xl font-semibold border-b border-blue-300    ">
                                         Storage</h2>
                                     <div>
                                         <div className="flex flex-wrap -mx-2 -mb-2">
                                             <button
-                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400   hover:text-blue-600    ">
                                                 256 GB
                                             </button>
                                             <button
-                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600      ">
                                                 112 GB
                                             </button>
                                             <button
-                                                className="px-4 py-2 mb-2 mr-2 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">
+                                                className="px-4 py-2 mb-2 mr-2 font-semibold border rounded-md hover:border-blue-400 hover:text-blue-600      ">
                                                 1 TB
                                             </button>
                                         </div>
@@ -215,7 +215,7 @@ const Detail = () => {
                                 </div>
                                 <div className="w-32 mb-8 ">
                                     <label for=""
-                                        className="w-full pb-1 text-xl font-semibold text-gray-700 border-b border-blue-300 dark:border-gray-600 dark:text-gray-400">Quantity</label>
+                                        className="w-full pb-1 text-xl font-semibold text-gray-700 border-b border-blue-300    ">Quantity</label>
                                     <div className="relative flex flex-row w-full h-10 mt-6 bg-transparent rounded-lg">
                                         <button onClick={handleMinus}
                                             className="w-20 h-full bg-gray-800">
@@ -233,10 +233,10 @@ const Detail = () => {
                                 </div>
                                 <div className="flex flex-wrap items-center gap-4">
                                     <button
-                                        className="w-full p-4 bg-blue-500 rounded-md lg:w-2/5 dark:text-gray-200 text-gray-50 hover:bg-blue-600 dark:bg-blue-500 dark:hover:bg-blue-700">
+                                        className="w-full p-4 bg-blue-500 rounded-md lg:w-2/5   text-gray-50 hover:bg-blue-600 dark:bg-blue-500  ">
                                         Add to cart</button>
                                     <button
-                                        className="flex items-center justify-center w-full p-4 text-blue-500 border border-blue-500 rounded-md lg:w-2/5 dark:text-gray-200 dark:border-blue-600 hover:bg-blue-600 hover:border-blue-600 hover:text-gray-100 dark:bg-blue-500 dark:hover:bg-blue-700 dark:hover:border-blue-700 dark:hover:text-gray-300">
+                                        className="flex items-center justify-center w-full p-4 text-blue-500 border border-blue-500 rounded-md lg:w-2/5   dark:border-blue-600 hover:bg-blue-600 hover:border-blue-600 hover:text-gray-100">
                                         Buy Now
                                     </button>
                                 </div>
