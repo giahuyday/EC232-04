b986cafe65dec9d5c2bffcb7d51d5d36f460d296
nminhdev <heomeco2014@gmail.com>
2023-08-10 11:31:35 +0700

    feat: add real data from db

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -3,61 +3,87 @@ import Axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { InputNumber } from 'antd'
 import { CiDeliveryTruck } from 'react-icons/ci'
-import { BsArrowRepeat } from 'react-icons/bs' 
-import { useSpring, animated } from "react-spring";
+import { BsArrowRepeat } from 'react-icons/bs'
+import { useSpring, animated } from 'react-spring'
 const Detail = () => {
   const { ItemID } = useParams()
   const [imgMain, setImgMain] = useState()
   const [amount, setAmount] = useState(1)
-  const [showMorePara, setShowMorePara] = useState(false)
-  const [showMoreAbout, setShowMoreAbout] = useState(false)
-  const [Product, SetProduct] = useState([])
+  const [product, setProduct] = useState({})
+  const [loading, setLoading] = useState(false)
   const onAmountChange = (value) => {
     setAmount(value)
-
   }
   const handAddCart = async () => {
     await Axios.post('http://localhost:3001/cartpost/add', {
-      Account: "Acc1",
+      Account: 'Acc1',
       itemId: ItemID,
-      Quantity: amount
+      Quantity: amount,
     })
     handleFlyInClick()
   }
-  useEffect(() => {
-    Axios.post(`http://localhost:3001/detail/${ItemID}`, {
-      ItemID: ItemID,
-    }).then((response) => {
-      SetProduct(response.data)
+  const fetchProductDetail = async () => {
+    try {
+      const res = await Axios.get(`http://localhost:3001/detail/${ItemID}`)
+      console.log('res: ', res)
+      const product = parseProductDetails(res.data)
+      setProduct(product)
+      console.log('Fetch successfull', product)
+    } catch (error) {
+      console.log('error ne`: ', error)
+    }
+  }
+  const parseProductDetails = (product) => {
+    let imagesLinks = []
+    const { Description, Name, Price, ProDucerID, Status } = product?.[0]
+    product.map((p, i) => {
+      const imgLink = p.Content
+      imagesLinks.push(imgLink)
     })
-    setImgMain('https://media.gamestop.com/i/gamestop/11108369-a2080ccd?$pdp$')
+    return {
+      Description,
+      Name,
+      Price,
+      ProDucerID,
+      Status,
+      imagesLinks,
+    }
+  }
+
+  useEffect(() => {
+    fetchProductDetail()
+    if (!product) setLoading(true)
+    else setLoading(false)
   }, [])
-  const listImages = ['https://media.gamestop.com/i/gamestop/11108369-a2080ccd?$pdp$', 'https://m.media-amazon.com/images/I/51JCqvlkqVL._AC_UF1000,1000_QL80_.jpg', 'https://media.gamestop.com/i/gamestop/11108369-a2080ccd?$pdp$', 'https://m.media-amazon.com/images/I/51JCqvlkqVL._AC_UF1000,1000_QL80_.jpg']
+  useEffect(() => {
+    setImgMain(product?.imagesLinks?.[0])
+    console.log('Product: ', product)
+  }, [product])
   function handleShowClickedImage(e) {
     setImgMain(e.target.src)
   }
-  const [isFlyIn, setIsFlyIn] = useState(false);
+  const [isFlyIn, setIsFlyIn] = useState(false)
 
   const handleFlyInClick = () => {
-    setIsFlyIn(true);
+    setIsFlyIn(true)
     setTimeout(() => {
-      setIsFlyIn(false);
-    }, 1000);
-  };
+      setIsFlyIn(false)
+    }, 1000)
+  }
 
   const flyInAnimation = useSpring({
-    transform: isFlyIn ? "translate(1500px,0)" : "translate(300px,200px)",
+    transform: isFlyIn ? 'translate(1500px,0)' : 'translate(300px,200px)',
     opacity: isFlyIn ? 1 : 0,
-    config: { duration: 1000 }, 
-  });
+    config: { duration: 1000 },
+  })
   return (
     <>
-        <animated.img src={imgMain} alt="" className="absolute w-[100px] h-[100px] object-contain"  style={flyInAnimation}/>
- 
+      <animated.img src={imgMain} alt="" className="absolute w-[100px] h-[100px] object-contain rounded-md" style={flyInAnimation} />
+
       {/* 3 columns responsive based on Grid */}
-      <div className="max-w-screen-2xl mx-auto p-5 grid sm:grid-cols-[1fr_4fr] md:grid-cols-[1fr_2fr] lg:grid-cols-[4fr_13fr_13fr] gap-x-[100px]">
+      <div className="max-w-screen-2xl mx-auto p-5 grid sm:grid-cols-[1fr_4fr] md:grid-cols-[1fr_2fr] lg:grid-cols-[4fr_13fr_13fr] gap-x-[100px] min-h-[70vh]">
         <div className="flex flex-col justify-around">
-          {listImages.map((item, index) => {
+          {product?.imagesLinks?.map((item, index) => {
             return (
               <div key={index} className="max-w-[170px] max-h-[138px]" onClick={handleShowClickedImage}>
                 <img src={item} alt="" className="object-cover mix-blend-color-burn" />
@@ -67,10 +93,9 @@ const Detail = () => {
         </div>
         <div className="flex">
           <img src={imgMain} alt="" className="w-full h-full object-contain" />
-        
         </div>
         <div className="flex flex-col col-span-2 lg:col-span-1">
-          <h3 className="text-[1.5rem] font-semibold leading-6 space tracking-[0.045rem] font-sans">{Product[0]?.Name}</h3>
+          <h3 className="text-[1.5rem] font-semibold leading-6 space tracking-[0.045rem] font-sans">{product.Name}</h3>
           <div className="flex py-6">
             <div className="flex items-center space-x-1">
               <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -91,10 +116,10 @@ const Detail = () => {
               <div className="pl-2 ">(150 reviews)</div>
             </div>
             <div className="block w-[1px] h-full mx-4 bg-black "></div>
-            <div className="flex items-center space-x-1 text-green-400">In Stock</div>
+            <div className="flex items-center space-x-1 text-green-400">{product.Status}</div>
           </div>
-          <div className="text-[1.5rem] tracking-[0.045rem] mb-4">$ 192.00</div>
-          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque commodi unde error hic ipsum voluptates facilis quia quam aliquam aspernatur architecto quo dicta, alias facere officiis repellat reprehenderit earum!</p>
+          <div className="text-[1.5rem] tracking-[0.045rem] mb-4">$ {product.Price}</div>
+          <p className="">{product.Description}</p>
           <div className="h-[1px] w-full my-4 bg-black "></div>
           <div className="flex gap-6">
             <p className="text-[20px]">Colours: </p>
@@ -116,15 +141,14 @@ const Detail = () => {
             <InputNumber min={1} max={100} defaultValue={amount} onChange={onAmountChange} size="large" className="self-stretch" />
             <button className="px-6 py-4  bg-rose-500 text-white select-none hover:bg-rose-600 transition-all active:scale-95">Buy now</button>
             <div className="">
-              <button class="ml-[15px] w-[200px] h-[55px] rounded-12 border-none bg-yellow-400 flex items-center justify-center cursor-pointer transition duration-500 overflow-hidden shadow-md relative group" onClick={handAddCart}>
-                <span class="absolute left-[-50px] w-30 h-30 bg-transparent rounded-full flex items-center justify-center overflow-hidden z-2 transition duration-500 group-hover:translate-x-[58px] group-hover:rounded-40">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart w-5 h-5">
+              <button className="ml-[15px] w-[200px] h-[55px] rounded-12 border-none bg-yellow-400 flex items-center justify-center cursor-pointer transition duration-500 overflow-hidden shadow-md relative group" onClick={handAddCart}>
+                <span className="absolute left-[-50px] w-30 h-30 bg-transparent rounded-full flex items-center justify-center overflow-hidden z-2 transition duration-500 group-hover:translate-x-[58px] group-hover:rounded-40">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" className="cart w-5 h-5">
                     <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
                   </svg>
                 </span>
-                <p class="h-full w-fit-content flex items-center justify-center text-gray-700 z-1 transition duration-500 group-hover:translate-x-10 group-hover:translate-y-0 group-hover:font-bold text-1.04em font-semibold">Add to Cart</p>
+                <p className="h-full w-fit-content flex items-center justify-center text-gray-700 z-1 transition duration-500 group-hover:translate-x-10 group-hover:translate-y-0 group-hover:font-bold text-1.04em font-semibold">Add to Cart</p>
               </button>
-
             </div>
             <div className="border rounded-md flex item-center p-2 cursor-pointer hover:bg-rose-500 transition-all active:scale-95 ml-auto">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-10 h-10 ">
@@ -153,6 +177,7 @@ const Detail = () => {
         </div>
       </div>
     </>
+    // <h1></h1>
   )
 }
 
